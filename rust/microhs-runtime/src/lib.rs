5188c946a5ae5ca7ba08d2bc4a7e2354257bb519
Matthias Pall Gissurarson <mpg@mpg.is>
2026-07-02 15:49:05 +0200

    Add Rust JS wrapper callback API

--- rust/microhs-runtime/src/lib.rs
+++ rust/microhs-runtime/src/lib.rs
@@ -2,4 +2,4 @@ pub mod parse;
 pub mod runtime;
 
 pub use parse::{ParseError, parse_program};
-pub use runtime::{EvalError, Node, NodeId, Program};
+pub use runtime::{EvalError, JsValue, Node, NodeId, Program};
