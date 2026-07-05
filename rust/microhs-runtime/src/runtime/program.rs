a6bf92a758cf48e36af7c033847f6b06de447da8
Matthias Pall Gissurarson <mpg@mpg.is>
2026-07-05 15:00:16 +0200

    Remove dead moving GC scaffold

--- rust/microhs-runtime/src/runtime/program.rs
+++ rust/microhs-runtime/src/runtime/program.rs
@@ -3,15 +3,11 @@ use super::*;
 
 mod bfile;
 mod core;
-#[cfg(feature = "moving-gc")]
-mod evacuate;
 mod eval;
 mod gc;
 mod handles;
 mod profile_hooks;
 mod public;
-#[cfg(any(test, feature = "moving-gc"))]
-mod remap;
 mod runtime_dispatch;
 mod serialize;
 mod values;
